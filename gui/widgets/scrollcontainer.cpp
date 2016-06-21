d62cc256deb0d0902de6ade257c28185caf6d8e3
Alexander Tkachev <alexander@tkachov.ru>
2016-06-21 14:51:50 +0600

    GUI: Remove _clippingArea from ScrollContainer

--- gui/widgets/scrollcontainer.cpp
+++ gui/widgets/scrollcontainer.cpp
@@ -73,8 +73,11 @@ void ScrollContainerWidget::init() {
 	_scrolledX = 0;
 	_scrolledY = 0;
 	_limitH = 140;
+<<<<<<< HEAD
 	_clippingArea = Common::Rect(0, 0, _w, _h);
 >>>>>>> 75117ff... GUI: Add ScrollContainer
+=======
+>>>>>>> 2ef853d... GUI: Remove _clippingArea from ScrollContainer
 	recalc();
 }
 
@@ -305,7 +308,6 @@ Widget *ScrollContainerWidget::findWidget(int x, int y) {
 }
 
 void ScrollContainerWidget::reflowLayout() {
-	_clippingArea = Common::Rect(0, 0, _w, _h);
 	recalc();
 	Widget::reflowLayout();
 }
