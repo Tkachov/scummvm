1baa4faea2576df42ccd60d400fc0520ea31e550
Alexander Tkachev <alexander@tkachov.ru>
2016-06-22 15:02:46 +0600

    GUI: Make PopUpWidget clip

--- gui/widget.cpp
+++ gui/widget.cpp
@@ -272,11 +272,8 @@ void StaticTextWidget::setAlign(Graphics::TextAlign align) {
 
 
 void StaticTextWidget::drawWidget() {
-	int px = _boss->getAbsX();
-	int py = _boss->getAbsY();
 	g_gui.theme()->drawTextClip(
-		Common::Rect(_x, _y, _x+_w, _y+_h),		
-		Common::Rect(px, py, px + _boss->getWidth(), py + _boss->getHeight()),
+		Common::Rect(_x, _y, _x+_w, _y+_h),	getBossClipRect(),
 		_label, _state, _align, ThemeEngine::kTextInversionNone, 0, true, _font
 	);
 }
@@ -316,12 +313,9 @@ void ButtonWidget::handleMouseDown(int x, int y, int button, int clickCount) {
 	setPressedState();
 }
 
-void ButtonWidget::drawWidget() {	
-	int px = _boss->getAbsX();
-	int py = _boss->getAbsY();
+void ButtonWidget::drawWidget() {
 	g_gui.theme()->drawButtonClip(
-		Common::Rect(_x, _y, _x + _w, _y + _h),
-		Common::Rect(px, py, px + _boss->getWidth(), py + _boss->getHeight()),
+		Common::Rect(_x, _y, _x + _w, _y + _h), getBossClipRect(),
 		_label, _state, getFlags()
 	);
 }
